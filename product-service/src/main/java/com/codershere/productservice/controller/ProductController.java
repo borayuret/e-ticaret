bf4ffa011138933d26ec92eb81dcf50fe7891aa4
Bora Yüret <bora.yuret@alchemy.com.tr>
2023-11-08 15:47:35 +0300

    Swagger ekledik.

--- product-service/src/main/java/com/codershere/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/codershere/productservice/controller/ProductController.java
@@ -1,18 +1,28 @@
 package com.codershere.productservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codershere.productservice.document.Product;
+import com.codershere.productservice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
 
 //    @GetMapping("/merhaba")
 //    public String selamla()
 //    {
 //        return "Merhaba Dünya";
 //    }
+
+    @PostMapping("/product")
+    public void createProduct(@RequestBody Product product)
+    {
+        productService.createProduct(product);
+    }
 
 
 }
--- product-service/src/main/java/com/codershere/productservice/service/ProductServiceImpl.java
+++ product-service/src/main/java/com/codershere/productservice/service/ProductServiceImpl.java
@@ -1,10 +1,19 @@
 package com.codershere.productservice.service;
 
+import com.codershere.productservice.document.Product;
+import com.codershere.productservice.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements ProductService{
 
+    @Autowired
+    private ProductRepository productRepository;
 
 
+    @Override
+    public void createProduct(Product product) {
+        productRepository.save(product);
+    }
 }
